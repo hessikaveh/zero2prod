a5ec41300a5b51c3db46d65604daec29eba847c3
hkaveh <hessamoddin.kaveh@cern.ch>
2024-02-23 21:53:48 +0100

    add the first test

---
+++ README.md
@@ -0,0 +1,12 @@
+Use the following commands
+
+```Bash
+cargo watch -x check -x test -x run
+
+cargo tarpaulin --ignore-tests
+cargo clippy
+cargo fmt
+cargo fmt -- --check
+cargo audit
+
+```
---
+++ tests/health_check.rs
@@ -0,0 +1,29 @@
+//! tests/health_check.rs
+
+use std::net::TcpListener;
+
+use tokio::spawn;
+use zero2prod::run;
+
+#[tokio::test]
+async fn test_health_check_works() {
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+
+    let response = client
+        .get(&format!("{}/health_check", &address))
+        .send()
+        .await
+        .expect("Faied to execute request.");
+
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port.");
+    let port = listener.local_addr().unwrap().port();
+    let server = run(listener).expect("Failed to bind address.");
+    let _ = spawn(server);
+    format!("http://127.0.0.1:{}", port)
+}
